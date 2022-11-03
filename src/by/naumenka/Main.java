864bbd71ebbdd9e5b7c64c9cf521571aa37bc5d9
Darya Naumenka <naumenka.daria@gmail.com>
2022-11-03 21:22:39 +0300

    commit

--- src/by/naumenka/Main.java
+++ src/by/naumenka/Main.java
@@ -1,50 +1,31 @@
 package by.naumenka;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class Main {
+    public static void main(String[] args) {
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        while (true) {
+        Scanner in = new Scanner(System.in);
 
-            Scanner in = new Scanner(System.in);
+        System.out.println("Введите число");
+        int number = in.nextInt();
+        int numberOfCitations = 0;
 
-            System.out.println("Введите число");
-
-            int number = in.nextInt();
-            System.out.println("Вы ввели число " + number);
-            if (number > 0 && number < 15) {
-                System.out.println("Группа 1");
-                for (int i = 0; i < 1; i++) {
-                    System.out.println("Не ешьте на ночь сырых помидоров! - Остап Бендер");
-                }
-                System.out.println("Хотите продолжить? Нажмите Enter, иначе введите 'выход' ");
-            } else if (number < 30 && number >= 15) {
-                System.out.println("Группа 2");
-                for (int i = 0; i < 2; i++) {
-                    System.out.println("Не ешьте на ночь сырых помидоров! - Остап Бендер");
-                }
-                System.out.println("Хотите продолжить? Нажмите Enter, иначе введите 'выход' ");
-            } else if (number >= 30) {
-                System.out.println("Группа 3");
-                for (int i = 0; i < 3; i++) {
-                    System.out.println("Не ешьте на ночь сырых помидоров! - Остап Бендер");
-                }
-                System.out.println("Хотите продолжить? Нажмите Enter, иначе введите 'выход' ");
-            } else {
-                System.out.println("Не в диапазоне, проверь введенное число");
-                System.out.println("Хотите проверить? Нажмите Enter, иначе введите 'выход' ");
-            }
-
-            String input;
-            String exit = "выход";
-            input = reader.readLine();
-            if (exit.equals(input)) break;
-
+        if (number > 0 && number < 15) {
+            System.out.println("Группа 1");
+            numberOfCitations = 1;
+        } else if (number < 30 && number >= 15) {
+            System.out.println("Группа 2");
+            numberOfCitations = 2;
+        } else if (number >= 30) {
+            System.out.println("Группа 3");
+            numberOfCitations = 3;
+        } else {
+            System.out.println("Не в диапазоне, проверь введенное число");
+        }
+        for (int i = 0; i < numberOfCitations; i++) {
+            System.out.println("Не ешьте на ночь сырых помидоров! - Остап Бендер");
         }
     }
 }
+
