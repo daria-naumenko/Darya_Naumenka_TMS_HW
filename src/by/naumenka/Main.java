deeef6a43559721221e28c78e56c2837666e6910
Darya Naumenka <naumenka.daria@gmail.com>
2022-11-03 19:50:22 +0300

    commit

--- src/by/naumenka/Main.java
+++ src/by/naumenka/Main.java
@@ -6,6 +6,7 @@ import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         while (true) {
