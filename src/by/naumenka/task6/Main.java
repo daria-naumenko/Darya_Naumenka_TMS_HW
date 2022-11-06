f742c29aae8001027c17c34923ac87c9a6fcbd3f
Darya Naumenka <naumenka.daria@gmail.com>
2022-11-06 22:39:01 +0300

    completed my sixth homework task

---
+++ src/by/naumenka/task6/Main.java
@@ -0,0 +1,12 @@
+package by.naumenka.task6;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        String[] array = {"Darya", "Dan", "Egor", "Alesya"};
+        System.out.println("Неотсортированный массив: " + Arrays.toString(array));
+        Arrays.sort(array);
+        System.out.println("Отсортированный массив: " + Arrays.toString(array));
+    }
+}
