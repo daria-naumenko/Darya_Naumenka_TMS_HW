2000c55088fc23065fafafc9dab22c5a97edb546
Darya Naumenka <naumenka.daria@gmail.com>
2022-11-06 22:03:25 +0300

    completed my second homework task

---
+++ src/by/naumenka/task2/Main.java
@@ -0,0 +1,29 @@
+package by.naumenka.task2;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Введите длину массива ");
+        Scanner input = new Scanner(System.in);
+        int size = input.nextInt();
+        int[] array = new int[size];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int) (Math.random() * 10);
+        }
+        System.out.println("Ваш массив " + Arrays.toString(array));
+        Arrays.sort(array);
+        System.out.println("Отсортировали массив: " + Arrays.toString(array));
+        System.out.println("min = " + array[0]);
+        System.out.println("max = " + array[size - 1]);
+
+        double sum = 0;
+        for (int var : array) {
+            sum += var;
+        }
+        double average = sum / size;
+        System.out.printf("Среднее значение чисел в массиве = %.2f", average);
+    }
+}
